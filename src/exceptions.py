f8504b3aff1c8572242833e0675d3eb37cd6ab55
Thomas Moenicke <tm@compunix.de>
2025-03-01 07:52:57 +0200

    Adding missing exceptions file

---
+++ src/exceptions.py
@@ -0,0 +1,15 @@
+"""
+Agent Assembly Line
+"""
+
+class DataLoadError(Exception):
+    """Exception raised for errors in the data loading process."""
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+class EmptyDataError(Exception):
+    """Exception raised when no data is loaded from the file."""
+    def __init__(self, filepath: str):
+        self.message = f"No text loaded, the file doesn't seem to contain any text."
+        super().__init__(self.message)
