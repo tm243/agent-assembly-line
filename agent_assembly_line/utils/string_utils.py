d7a5b9fbd8a3d1e4996d21d577fbe1056d99cd59
Thomas Moenicke <tm@compunix.de>
2026-01-31 10:09:41 +0200

    Moving string logic to string_utils, removing dependency

---
+++ agent_assembly_line/utils/string_utils.py
@@ -0,0 +1,9 @@
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0)."""
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError(f"invalid truth value {val!r}")
